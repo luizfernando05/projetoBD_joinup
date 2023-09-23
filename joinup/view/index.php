a3a9556b30b9cbc74341ac17f1abac6d65a8d870
luizfernando05 <luiz.fernando.lf2016@outlook.com>
2023-09-23 12:28:30 -0300

    v.0.0.8 - correção da consulta sql

--- joinup/view/index.php
+++ joinup/view/index.php
@@ -80,61 +80,54 @@
 
         <div class="cards-oportunidades">
 
-            <?php
-            include_once('../model/config.php');
+        <?php
+        include_once('../model/config.php');
 
-            $query = "
-            SELECT o.nomeOportunidade, e.nomeEmpresa, t.tipo, o.cidade, o.estado, r.requisito, o.dataInicio, o.dataFim, e.setorEmpresa, e.emailEmpresa, e.telefoneEmpresa
-            FROM sistema.oporEmp AS oe 
-            INNER JOIN sistema.oportunidade AS o ON oe.idOportunidade = o.idOportunidade 
-            INNER JOIN sistema.empresa AS e ON oe.cnpjEmpresa = e.cnpjEmpresa 
-            INNER JOIN sistema.tipoOportunidade AS t ON o.idOportunidade = t.idOportunidade 
-            INNER JOIN sistema.requisitoOportunidade AS r ON o.idOportunidade = r.idOportunidade;
+        try {
+            $query1 = "
+            SELECT o.nomeoportunidade, e.nomeempresa, t.tipo, o.cidade, o.estado, o.datainicio, o.datafim, e.setorempresa, e.emailempresa, e.telefoneempresa, o.linkins, string_agg(r.requisito, ', ') AS requisitos
+            FROM sistema.oporemp AS oe
+            INNER JOIN sistema.oportunidade AS o ON oe.idoportunidade = o.idoportunidade
+            INNER JOIN sistema.empresa AS e ON oe.cnpjempresa = e.cnpjempresa
+            INNER JOIN sistema.tipooportunidade AS t ON o.idoportunidade = t.idoportunidade
+            LEFT JOIN sistema.requisitooportunidade AS r ON o.idoportunidade = r.idoportunidade
+            GROUP BY o.nomeoportunidade, e.nomeempresa, t.tipo, o.cidade, o.estado, o.datainicio, o.datafim, e.setorempresa, e.emailempresa, e.telefoneempresa, o.linkins;
             ";
 
-            try {
-                $stmt = $conn->query($query);
-                while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-                    echo '<div class="card-opor">';
-                    echo '<h3 class="font-1-m-b color-c12">' . $row['nomeoportunidade'] . '</h3>';
-            
-                    echo '<div class="info-gerais">';
-                    echo '<span class="font-1-xs color-c12">' . $row['nomeempresa'] . '</span>';
-                    echo '<span class="font-1-xs color-c12">' . $row['tipo'] . '</span>';
-                    echo '<span class="font-1-xs color-c12">' . $row['cidade'] . ', ' . $row['estado'] . '</span>';
-                    echo '</div>';
-            
-                    echo '<div class="cards-opor-info">';
-                    echo '<div class="opor-preRequi">';
-                    echo '<h4 class="font-1-s color-c12">Pré-requisitos:</h4>';
- 
-                    $requisitos = explode(', ', $row['requisito']);
-                    foreach ($requisitos as $requisito) {
-                        echo '<span class="font-2-xs color-c12">' . $requisito . '</span>';
-                    }
-                    echo '</div>';
-            
-                    echo '<div class="opor-datas">';
-                    echo '<h4 class="font-1-s color-c12">Datas da seleção:</h4>';
-                    echo '<span class="font-2-xs color-c12">Início da seleção: ' . $row['datainicio'] . '</span>';
-                    echo '<span class="font-2-xs color-c12">Fim da seleção: ' . $row['datafim'] . '</span>';
-                    echo '</div>';
-            
-                    echo '<div class="info-emp">';
-                    echo '<h4 class="font-1-s color-c12">Informações da empresa:</h4>';
-                    echo '<span class="font-2-xs color-c12">Área de atuação: ' . $row['setorempresa'] . '</span>';
-                    echo '<span class="font-2-xs color-c12">E-mail para contato: ' . $row['emailempresa'] . '</span>';
-                    echo '<span class="font-2-xs color-c12">Telefone para contato: ' . $row['telefoneempresa'] . '</span>';
-                    echo '</div>';
-            
-                    echo '<a class="btn" href="' . $row['linkins'] . '">CANDIDATAR-SE</a>';
-                    echo '</div>'; 
-                }
-            } catch (PDOException $e) {
-                echo "Erro na consulta: " . $e->getMessage();
-            }
-            ?>
+            $stmt1 = $conn->query($query1);
 
+            while ($row = $stmt1->fetch(PDO::FETCH_ASSOC)) {
+                echo '<div class="card-opor">';
+                echo '<h3 class="font-1-m-b color-c12">' . $row['nomeoportunidade'] . '</h3>';
+                echo '<div class="info-gerais">';
+                echo '<span class="font-1-xs color-c12">' . $row['nomeempresa'] . '</span>';
+                echo '<span class="font-1-xs color-c12">' . $row['tipo'] . '</span>';
+                echo '<span class="font-1-xs color-c12">' . $row['cidade'] . ', ' . $row['estado'] . '</span>';
+                echo '</div>';
+                echo '<div class="cards-opor-info">';
+                echo '<div class="opor-preRequi">';
+                echo '<h4 class="font-1-s color-c12">Pré-requisitos:</h4>';
+                echo '<span class="font-2-xs color-c12">' . $row['requisitos'] . '</span>';
+                echo '</div>';
+                echo '<div class="opor-datas">';
+                echo '<h4 class="font-1-s color-c12">Datas da seleção:</h4>';
+                echo '<span class="font-2-xs color-c12">Início da seleção: ' . $row['datainicio'] . '</span>';
+                echo '<span class="font-2-xs color-c12">Fim da seleção: ' . $row['datafim'] . '</span>';
+                echo '</div>';
+                echo '<div class="info-emp">';
+                echo '<h4 class="font-1-s color-c12">Informações da empresa:</h4>';
+                echo '<span class="font-2-xs color-c12">Área de atuação: ' . $row['setorempresa'] . '</span>';
+                echo '<span class="font-2-xs color-c12">E-mail para contato: ' . $row['emailempresa'] . '</span>';
+                echo '<span class="font-2-xs color-c12">Telefone para contato: ' . $row['telefoneempresa'] . '</span>';
+                echo '</div>';
+                echo '<a class="btn" href="' . $row['linkins'] . '">CANDIDATAR-SE</a>';
+                echo '</div>';
+                echo '</div>';
+            }
+        } catch (PDOException $e) {
+            echo "Erro na consulta: " . $e->getMessage();
+        }
+        ?>
         </div>
 
     </div>
